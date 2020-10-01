c10af4ebeb1adc9795ab0de3db780f7d9116ba2e
WhiteHornet <43600081+WhiteHornet@users.noreply.github.com>
2020-10-01 18:58:27 +0200

    'helloWorld.java'

---
+++ helloWorld1.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package helloWorld;
+
+/**
+ * @author LPOTT
+ *
+ */
+import java.util.*;
+public class helloWorld1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Git is Awesome");
+		
+	}
+
+}
